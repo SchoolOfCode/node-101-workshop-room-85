2d532553882154e81c89ebf5fef5637ed3fcc405
ana-maria-b <kaonashi2@hotmail.com>
2021-12-06 20:11:55 +0300

    node begins

---
+++ index.js
@@ -0,0 +1,23 @@
+let myCollection = [
+  {
+    name: "School of Code mug",
+    count: 1,
+    whatILike: "It has my cute pixel character on it!",
+  },
+  {
+    name: "School of Code hat",
+    count: 2,
+    whatILike: "An often overlooked fashion accessory",
+  },
+  {
+    name: "School of Code pillow",
+    count: 1,
+    whatILike: "Eat. Sleep. Code. Repeat :)",
+  },
+];
+console.log(myCollection);
+
+function describeItem(item) {
+  console.log(myCollection[0].count);
+}
+
